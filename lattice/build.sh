89fe46470a2a3c92a7a012cf92644fd909be6482
Felix <felix@niederer.dev>
2025-05-25 21:30:40 +0200

    added make

--- lattice/build.sh
+++ lattice/build.sh
@@ -1,7 +1,7 @@
-bender script flist-plus -t fpga -t rtl --define SYNTHESIS=1 -t ecp5 > croc.flist
 mkdir -p _build
+bender script flist-plus -t fpga -t rtl --define SYNTHESIS=1 -t ecp5 > _build/croc.flist
 yosys -s run_yosys.ys
 
 nextpnr-ecp5 --25k --package CABGA256 --speed 6 --json _build/hardware.json --textcfg _build/hardware.config --report _build/hardware.pnr --lpf constraints/iCESugar-Pro.lpf
 
-apio raw 'ecppack --svf _build/hardware.svf _build/hardware.config _build/hardware.bit'
+ecppack --svf _build/hardware.svf _build/hardware.config _build/hardware.bit
