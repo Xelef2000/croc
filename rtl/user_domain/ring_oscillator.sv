8ef8f5e20e793d405351c5836e266e8f6c32813b
Felix <felix@niederer.dev>
2025-06-30 21:04:04 +0200

    fixed sim

--- rtl/user_domain/ring_oscillator.sv
+++ rtl/user_domain/ring_oscillator.sv
@@ -2,16 +2,33 @@ module ring_oscillator (
     output wire osc_out
 );
 
+`ifdef VERILATOR
+    // Simulatable version: use a toggle register driven by a clock
+    reg dummy_osc = 0;
+
+    reg [7:0] counter = 0;
+    always @(posedge dummy_clk) begin
+        counter <= counter + 1;
+        if (counter == 8'd0)
+            dummy_osc <= ~dummy_osc;
+    end
+
+    reg dummy_clk = 0;
+    always #50 dummy_clk = ~dummy_clk; // 10MHz simulated clock
+
+    assign osc_out = dummy_osc;
+
+`else
+    // Real ring oscillator (for synthesis/silicon)
     wire n1, n2, n3, n4, n5;
 
-    // Inverter chain with odd number of stages (5)
     assign n1 = ~n5;
     assign n2 = ~n1;
     assign n3 = ~n2;
     assign n4 = ~n3;
     assign n5 = ~n4;
 
-    // Output from one of the nodes
     assign osc_out = n3;
+`endif
 
 endmodule
