26cdc368b8dd82b2154f7b7c28358aa167fe425b
Felix <felix@niederer.dev>
2025-07-10 16:12:40 +0200

    rom fix

--- rtl/user_domain.sv
+++ rtl/user_domain.sv
@@ -111,9 +111,7 @@ module user_domain import user_pkg::*; import croc_pkg::*; #(
   obi_spi_rom #(
     .ObiCfg      ( SbrObiCfg     ),
     .obi_req_t   ( sbr_obi_req_t ),
-    .obi_rsp_t   ( sbr_obi_rsp_t ),
-    .BaseAddr    ( UserRomAddrOffset   ), // 32'h2000_0000
-    .Size        ( UserRomAddrRange    )  // 32'h0000_1000
+    .obi_rsp_t   ( sbr_obi_rsp_t )
   ) i_user_rom (
     .clk_i,
     .rst_ni,
