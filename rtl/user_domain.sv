b4a04c74491a277b8a682e072439b72a729d2e09
Felix <felix@niederer.dev>
2025-07-10 16:17:32 +0200

    fix c

--- rtl/user_domain.sv
+++ rtl/user_domain.sv
@@ -46,6 +46,11 @@ module user_domain import user_pkg::*; import croc_pkg::*; #(
   sbr_obi_req_t [NumDemuxSbr-1:0] all_user_sbr_obi_req;
   sbr_obi_rsp_t [NumDemuxSbr-1:0] all_user_sbr_obi_rsp;
 
+
+  // ROM Subordinate Bus
+  sbr_obi_req_t user_rom_obi_req;
+  sbr_obi_rsp_t user_rom_obi_rsp;
+
   // Rand Subordinate Bus
   sbr_obi_req_t user_Rand_obi_req;
   sbr_obi_rsp_t user_Rand_obi_rsp;
@@ -59,6 +64,8 @@ module user_domain import user_pkg::*; import croc_pkg::*; #(
   assign all_user_sbr_obi_rsp[UserError] = user_error_obi_rsp;
   assign user_Rand_obi_req                = all_user_sbr_obi_req[UserRand];
   assign all_user_sbr_obi_rsp[UserRand]   = user_Rand_obi_rsp;
+  assign user_rom_obi_req                = all_user_sbr_obi_req[UserRom];
+  assign all_user_sbr_obi_rsp[UserRom]   = user_rom_obi_rsp;
 
 
   //-----------------------------------------------------------------------------------------------
@@ -115,8 +122,8 @@ module user_domain import user_pkg::*; import croc_pkg::*; #(
   ) i_user_rom (
     .clk_i,
     .rst_ni,
-    .obi_req_i  ( user_sbr_obi_req_i ),
-    .obi_rsp_o  ( user_sbr_obi_rsp_o )
+    .obi_req_i  ( user_rom_obi_req ),
+    .obi_rsp_o  ( user_rom_obi_rsp )
   ); 
 
 
